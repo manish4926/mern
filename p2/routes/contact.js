7065c047045fd12247e2c9716248d57892ad4e6f
Manish Arora <manish.arora@payworldindia.com>
2023-12-03 20:19:35 +0530

    updated

--- p2/routes/contact.js
+++ p2/routes/contact.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const ContactObj = require('./../controller/Contact');
+const ContactObj = require('./../controller/ContactController');
 
 router.route('/').get(ContactObj.getContacts);
 
