069d13cf315991acbafff1e0298b64b8a3cb4395
Manish Arora <manish.arora@payworldindia.com>
2023-12-06 23:32:40 +0530

    old removed

--- p1-task-manager/middleware/illuminate.js
+++
@@ -1,7 +0,0 @@
-// This middleware is used to check multiple things like route available, auth header,
-
-const illuminateIdentifier = () => {
-    // return res.status(500).json({ msg: error });
-}
-
-module.exports = illuminateIdentifier;
--- p1-task-manager/middleware/routeidentifier.js
+++
@@ -1,3 +0,0 @@
-const routeIdentifier = (req, res) => res.status(404).json({ msg: "Route Not Found" });
-
-module.exports = routeIdentifier;
