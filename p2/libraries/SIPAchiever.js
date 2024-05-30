37eeeb58db4b661b2a8bf7a84b2068372990c575
Manish Arora <manish.arora@payworldindia.com>
2024-05-30 22:41:21 +0530

    updated

--- p2/libraries/SIPAchiever.js
+++ p2/libraries/SIPAchiever.js
@@ -1,10 +1,10 @@
 const { header } = require("express-validator");
 const asyncHandler = require('express-async-handler');
+const { end } = require("pdfkit");
 
 class SIPAchiever {
 
-    constructor(startDate) {
-        this.startDate = startDate;
+    constructor() {
         this.currentDate = new Date();
         //this.callBackYears = 10; //20 //Will update this in request data
         //Get Last 10 Years (Because 20 Years is much higher at beginning)
@@ -21,15 +21,40 @@ class SIPAchiever {
         let stepup_in_month = req.body.stepup_in_month;
         let stepuppercent = req.body.stepup_percent;
         let startdate = req.body.start_date;
+        let currentWealth = req.body.currentWealth;
+        let data = [];
+        
 
-        let data = this.calculateSIP(startdate, monthlyInvestment, interestRate, years, stepupamount, stepup_in_month, stepuppercent);
+        let monthlyInvestmentArr = [500,1000, 2000,5000,10000,15000];
+        let stepuppercentArr = [0, 5, 10, 15, 20];
 
-        return this.success_response(req, res, data);
+        for(let i = 0; i < monthlyInvestmentArr.length; i++) {
+            //For Loop for CallBackYears
+            
+            for(let j = 0; j < callBackYears; j++) {
+                let newStartDate = new Date(startdate);    
+                newStartDate.setFullYear(newStartDate.getFullYear() - j);
+
+                //For Loop for Step Up Api Data
+                for(let k = 0; k < stepuppercentArr.length; k++) {
+                    monthlyInvestment = monthlyInvestmentArr[i];
+                    stepuppercent = stepuppercentArr[k];
+                    //data.push([monthlyInvestmentArr[i], newStartDate.toDateString(), stepuppercentArr[k]]);        
+                    let response = this.calculateSIP(currentWealth, newStartDate, monthlyInvestment, interestRate, years, stepupamount, stepup_in_month, stepuppercent);
+                    data.push(response);
+                }
+            }           
+            
+        }
+
+        return data;
     });
 
-    calculateSIP = (startdate, monthlyInvestment, interestRate, years, stepupamount = 0, stepup_in_month  = 12, stepuppercent = 0 ) => {
+    calculateSIP = (currentWealth, startdate, monthlyInvestment, interestRate, years, stepupamount = 0, stepup_in_month  = 12, stepuppercent = 0 ) => {
+        let firstDate = new Date(startdate);
         startdate = new Date(startdate);
         startdate.setMonth(startdate.getMonth());
+        
 
         let totalMonths = years * 12;
         let monthlyRate = interestRate / 12 / 100;
@@ -38,6 +63,7 @@ class SIPAchiever {
         let totalAmount = 0;
         let totalInterest = 0;
         let data = [];
+        
         for (let i = 1; i <= totalMonths; i++) {
             //Step Up
             if (i % stepup_in_month === 0) {
@@ -57,17 +83,31 @@ class SIPAchiever {
             totalAmount += interest;
             //console.log(`Month ${i}: Total amount = ${totalAmount.toFixed(2)}`);
             startdate.setMonth(startdate.getMonth() + 1);
-            data.push({
-                'month': i,
-                'current_date': startdate.toDateString(),
-                'monthly_investment': monthlyInvestment,
-                'invested': finalInvestedAmount,
-                'total': totalAmount.toFixed(2),
-                'roi': interestRate,
-                'interest': interest,
-                'total_interest': totalInterest,
-            });
+            
+            if(totalAmount >= currentWealth && (i%12 == 0)) {
+                let diffBalance = (totalAmount - currentWealth+1).toFixed(0);
+                let yearAchieved = i/12;
+                let message = "Add "+ diffBalance + " amount to achieve " + yearAchieved + " years target Investement started on " + firstDate.toDateString();
+                if(stepuppercent > 0) {
+                    message = message + " with stepup percentage of "+ stepuppercent;
+                }
+                return {
+                    'month': i,
+                    'start_on' : firstDate.toDateString(),
+                    'current_date': startdate.toDateString(),
+                    'monthly_investment': monthlyInvestment,
+                    'invested': finalInvestedAmount,
+                    'total': totalAmount.toFixed(2),
+                    'roi': interestRate,
+                    'interest': interest.toFixed(2),
+                    'total_interest': totalInterest.toFixed(2),
+                    'stepup_percentage': stepuppercent,
+                    'message' : message
+                };
+            }
+            
         }
+
         return data;
     };
 }
